58a40e77f40e5bf4ec1cf4338c92bdff4cae6359
ujeans <dbwlsxkal95@gmail.com>
2024-07-08 15:28:39 +0900

    feat: react-router-dom 적용

--- react-twitter/src/App.tsx
+++ react-twitter/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>home</h1>} />
+      <Route path="/posts" element={<h1>post list</h1>} />
+      <Route path="/posts/:id" element={<h1>post detail</h1>} />
+      <Route path="/posts/new" element={<h1>post new</h1>} />
+      <Route path="/posts/edit/:id" element={<h1>post edit</h1>} />
+      <Route path="/profile" element={<h1>profile</h1>} />
+      <Route path="/profile/edit" element={<h1>profile edit</h1>} />
+      <Route path="/notifications" element={<h1>notifications</h1>} />
+      <Route path="search/" element={<h1>search</h1>} />
+      <Route path="/users/login" element={<h1>login</h1>} />
+      <Route path="/users/signup" element={<h1>signup</h1>} />
+      <Route path="*" element={<Navigate replace to="/" />} />
+    </Routes>
   );
 }
 
