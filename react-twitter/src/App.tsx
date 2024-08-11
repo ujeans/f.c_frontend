8ed7872f46a45d2548af320cf030d21bab3cc893
ujeans <dbwlsxkal95@gmail.com>
2024-08-11 20:24:06 +0900

    파일 올리기

---
+++ react-twitter/src/App.tsx
@@ -0,0 +1,12 @@
+import Router from "components/Router";
+import { Layout } from "components/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <Router />
+    </Layout>
+  );
+}
+
+export default App;
