8a8a2a020e59134a5413b50c31ee5a79aebcee86
ujeans <dbwlsxkal95@gmail.com>
2024-08-11 20:31:41 +0900

    터미널 테스트

--- mcard/src/App.tsx
+++ mcard/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactdsdfsdf
+          Learn Reactdsdfsdfdsdsds
         </a>
       </header>
     </div>
