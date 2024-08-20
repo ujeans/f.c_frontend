e8ac5def73a3715ac947c35693b7b126e4e78746
ujeans <dbwlsxkal95@gmail.com>
2024-08-20 22:29:08 +0900

    firebase에 Mock데이터 추가하기

--- mcard/src/App.tsx
+++ mcard/src/App.tsx
@@ -1,62 +1,16 @@
-import Alert from "./components/shared/Alert";
-import Button from "./components/shared/Button";
-import Input from "./components/shared/Input";
-import Text from "./components/shared/Text";
-import TextField from "./components/shared/TextField";
-import { useAlertContext } from "./contexts/AlertContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import TestPage from "./pages/Test";
 
 function App() {
-  const { open } = useAlertContext();
-
   return (
-    <div>
-      <Text typography="t1" display="block" color="red">
-        t1
-      </Text>
-      <Text typography="t2" color="blue">
-        t2
-      </Text>
-      <Text typography="t3">t3</Text>
-      <Text typography="t4">t4</Text>
-      <Text typography="t5">t5</Text>
-
-      <div style={{ height: 10, width: "100%", background: "#efefef" }}></div>
-
-      <Button>클릭하세요</Button>
-      <Button color="success">클릭하세요</Button>
-      <Button color="error">클릭하세요</Button>
-      <Button color="error" weak={true}>
-        클릭하세요
-      </Button>
-      <Button full={true}>클릭하세요</Button>
-      <Button full={true} disabled={true}>
-        클릭하세요
-      </Button>
-
-      <div style={{ height: 10, width: "100%", background: "#efefef" }}></div>
-
-      <Input placeholder="로그인" aria-invalid={false} />
-      <Input placeholder="회원가입" aria-invalid={true} />
-
-      <TextField label="아이디" />
-      <TextField label="회원가입" hasError={true} />
-
-      <div style={{ height: 10, width: "100%", background: "#efefef" }}></div>
-
-      <Button
-        onClick={() => {
-          open({
-            title: "카드 신청 완료",
-            description: "내역페이지에서 확인해주세요",
-            onButtonClick: () => {
-              //
-            },
-          });
-        }}
-      >
-        Alert 오픈
-      </Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/test" Component={TestPage} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- mcard/src/components/shared/Flex.tsx
+++ mcard/src/components/shared/Flex.tsx
@@ -1,17 +1,24 @@
 import styled from "@emotion/styled";
 import { CSSProperties } from "react";
+import { css, SerializedStyles } from "@emotion/react"; // css도 추가
 
 interface FlexProps {
   align?: CSSProperties["alignItems"];
   justify?: CSSProperties["justifyContent"];
   direction?: CSSProperties["flexDirection"];
+  css?: SerializedStyles; // 이 부분을 추가
 }
 
-const Flex = styled.div<FlexProps>(({ align, justify, direction }) => ({
-  display: "flex",
-  alignItems: align,
-  justifyContent: justify,
-  flexDirection: direction,
-}));
+const Flex = styled.div<FlexProps>(
+  ({ align, justify, direction, css: cssProp }) => [
+    {
+      display: "flex",
+      alignItems: align,
+      justifyContent: justify,
+      flexDirection: direction,
+    },
+    cssProp, // 추가된 css prop을 적용
+  ]
+);
 
 export default Flex;
