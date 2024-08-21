71d30d2dc6b9da5642fd58ec976486d11171ae61
ujeans <dbwlsxkal95@gmail.com>
2024-08-21 17:19:48 +0900

    banner 스와이프 구현

--- mcard/src/pages/Home.tsx
+++ mcard/src/pages/Home.tsx
@@ -1,27 +1,14 @@
-import { useEffect } from "react";
-
+import AdBanners from "../components/home/AdBanners";
 import Top from "../components/shared/Top";
 
-import { getCards } from "../remote/card";
-import { getAdBanners } from "../remote/adBanner";
-
 function HomePage() {
-  useEffect(() => {
-    getCards().then(res => {
-      console.log("res", res);
-    });
-
-    getAdBanners().then(res => {
-      console.log("res", res);
-    });
-  }, []);
-
   return (
     <div>
       <Top
         title="혜택 좋은 카드"
         subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤어요"
       />
+      <AdBanners />
     </div>
   );
 }
